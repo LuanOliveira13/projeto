872b9f88462423078d533e06650bc0fcfb0e6537
Luan Oliveira <luanhv013@gmail.com>
2024-01-14 23:33:52 -0300

    Projetos

---
+++ conta.py
@@ -0,0 +1,87 @@
+﻿import random
+from typing import Optional
+
+class Cliente:
+    def __init__(self, nome):
+        self.nome = nome
+        self.conta = Conta()
+        self.estoque = Estoque()
+
+    def transferir(self, valor: float):
+        self.conta.depositar(valor)
+
+    def adicionar_produto(self, produto):
+        self.estoque.adicionar_produto(produto)
+
+    def vender_produto(self, nome: str, quantidade: int):
+        produto = self.estoque.pesquisar_produto(nome)
+        if produto and produto.quantidade >= quantidade:
+            self.conta.sacar(produto.calcular_total(quantidade))
+            produto.quantidade -= quantidade
+        else:
+            print("Produto não encontrado ou quantidade insuficiente.")
+
+class Conta:
+    def __init__(self, numero=None, saldo=0.0):
+        self.numero = numero or self.gerar_numero()
+        self.saldo = saldo
+
+    def depositar(self, valor: float):
+        self.saldo += valor
+        print(f"Depósito de R${valor} realizado na conta {self.numero}.")
+
+    def sacar(self, valor: float):
+        if self.saldo >= valor:
+            self.saldo -= valor
+            print(f"Saque de R${valor} realizado na conta {self.numero}.")
+        else:
+            print("Saldo insuficiente.")
+            
+    def mostra_saldo(self):
+        print(f"Saldo atual: R${self.saldo}")
+
+    @staticmethod
+    def gerar_numero() -> int:
+        return random.randint(100000, 999999)
+
+class Produto:
+    def __init__(self, nome: str, preco: float, quantidade: int):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade = quantidade
+
+    def calcular_total(self, quantidade: int) -> float:
+        return self.preco * quantidade
+
+class Estoque:
+    def __init__(self):
+        self.produtos = []
+
+    def adicionar_produto(self, produto: Produto):
+        self.produtos.append(produto)
+
+    def remover_produto(self, produto: Produto):
+        self.produtos.remove(produto)
+
+    def pesquisar_produto(self, nome: str) -> Optional[Produto]:
+        for produto in self.produtos:
+            if produto.nome == nome:
+                return produto
+        return None
+
+    def exibir_estoque(self) -> None:
+        for produto in self.produtos:
+            print(f"Nome: {produto.nome} | Preço: R${produto.preco} | Quantidade: {produto.quantidade}")
+
+# Exemplo de uso
+cliente = Cliente("João")
+cliente.conta.depositar(500)
+cliente.adicionar_produto(Produto("Lápis", 0.5, 10))
+cliente.adicionar_produto(Produto("Caderno", 20, 5))
+
+cliente.vender_produto("Lápis", 2)
+cliente.vender_produto("Caneta", 1) # Produto não encontrado
+
+cliente.conta.sacar(100)
+cliente.conta.mostra_saldo()
+cliente.estoque.exibir_estoque()
---
+++ partida.py
@@ -0,0 +1,126 @@
+﻿import random
+
+from typing import List
+
+class Jogador:
+    def __init__(self, nome: str, habilidade: int, estrela: bool = False) -> None:
+        self.nome = nome
+        self.habilidade = habilidade
+        self.estrela = estrela
+
+    def __str__(self):
+        return f"{self.nome} (habilidade: {self.habilidade}, Star: {self.estrela})"
+
+class Time:
+    def __init__(self, nome: str, jogadores: List[Jogador]) -> None:
+        self.nome = nome
+        self.jogadores = jogadores
+
+    def __str__(self) -> str:
+        return f"{self.nome} - Star: {self.jogadores[0].nome}"
+
+def mostra_time(time: Time) -> None:
+    print(f"\n{time.nome} Roster:")
+    for i, Jogador in enumerate(time.jogadores):
+        print(f"{i+1}. {Jogador}")
+
+def play_basketball() -> None:
+    times: List[Time] = [
+        Time("Golden State Warriors", [
+            Jogador("Curry", 92, estrela=True),
+            Jogador("klay", 80),
+            Jogador("Kuminga", 80),
+            Jogador("Green", 85),
+            Jogador("Dario", 60),
+        ]),
+        Time("Los Angeles Lakers", [
+            Jogador("Russel", 70),
+            Jogador("Austin", 76),
+            Jogador("Lebron", 89 , estrela=True),
+            Jogador("Hachimura", 70),
+            Jogador("Davis", 90),
+        ]),
+        Time("Milhaucke Bucks", [
+            Jogador("Lillard", 88),
+            Jogador("Middleton", 70),
+            Jogador("Beasley", 73),
+            Jogador("Lopez", 75),
+            Jogador("Antepokoumpo", 90 , estrela=True),
+        ]),
+        Time("Boston Celtics", [
+            Jogador("Holiday", 78),
+            Jogador("Brown", 80),
+            Jogador("Tatum,", 86 , estrela=True),
+            Jogador("Horford", 78),
+            Jogador("Porzings", 80),
+        ]),
+    ]
+
+    print("Vamos jogar!")
+    mostra_time(times[0])
+    mostra_time(times[1])
+    mostra_time(times[2])
+    mostra_time(times[3])
+
+    time_usuario = int(input("Escolha seu time (0-3): "))
+    time_oponente = int(input("Escolha um oponente (0-3, excluindo seu time): "))
+
+    time_atual = times[time_usuario]
+    oponente = times[time_oponente]
+
+    print(f"\n{time_atual.nome} vs {oponente.nome}")
+
+    jogador_atual = 0
+    pontuacao_adversario = 0
+    pontuacao_usuario = 0
+    posse_de_bola = True
+
+    while pontuacao_adversario and pontuacao_usuario < 12:
+        if posse_de_bola:
+            print(f"\n{time_atual.nome} vem. {time_atual.jogadores[jogador_atual]} com a bola.")
+            action = input("Escolha uma ação (passe, falta, infiltracao, arremesso): ").lower()
+
+            if action == "passe":
+                if jogador_atual < len(time_atual.jogadores) - 1:
+                    jogador_atual += 1
+                else:
+                    jogador_atual = 0
+            elif action == "falta":
+                if random.randint(1, 100) <= time_atual.jogadores[jogador_atual].habilidade:
+                    pontuacao_usuario += 1
+                    print(f"Primeiro lance livre convertido. {pontuacao_usuario}")
+                if random.randint(1, 100) <= time_atual.jogadores[jogador_atual].habilidade:
+                    pontuacao_usuario += 1
+                    print(f"Segundo lance livre convertido. {pontuacao_usuario}")
+                else:
+                    print("Errou!")
+                posse_de_bola = False
+            elif action == "infiltracao":
+                if random.randint(1, 100) <= time_atual.jogadores[jogador_atual].habilidade:
+                    pontuacao_usuario += 2
+                    print(f"Cesta. Pontuação: {pontuacao_usuario}")
+                else:
+                    print("Errou!")
+                posse_de_bola = False
+            elif action == "arremesso":
+                if random.randint(1, 100) <= time_atual.jogadores[jogador_atual].habilidade:
+                    pontuacao_usuario += 3
+                    print(f"Para três pontos! Pontuação: {pontuacao_usuario}")
+                else:
+                    print("Turnover!")
+                posse_de_bola = False
+            else:
+                print("Ação inválida. Tente novamente.")
+        else:
+            print(f"\n{oponente.nome} com a bola.")
+            if random.randint(1, 100) <= oponente.jogadores[0].habilidade:
+                pontuacao_adversario += random.randint(1, 3)
+                print(f"O oponente faz a cesta. Pontuação: {pontuacao_adversario}")
+            else:
+                print("Turnover do adversario")
+            posse_de_bola = True
+
+    print(f"\n{time_atual.nome} ganhou com a pontução de {pontuacao_usuario}!")
+
+
+play_basketball()
